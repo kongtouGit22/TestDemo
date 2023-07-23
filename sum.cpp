e553bca9104f304cc7df7960eeaa106d126b72d9
kongtouGit22 <kongtouGit22@outlook.com>
2023-07-23 14:49:03 +0800

    add sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,21 @@
+
+int sum(int i, int j){
+
+	return i + j;
+}
+
+float sum(float i, float j){
+
+	return i + j;
+}
+
+double sum(double i, double j){
+
+	return i + j;
+}
+
+long int sum(long int i, long int j){
+
+	return i + j;
+}
+
